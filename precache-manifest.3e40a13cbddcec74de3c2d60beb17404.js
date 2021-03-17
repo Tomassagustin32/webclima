e4360efaf4c86abdbb3ccd61522719337434d240
tomassagustin32 <tomassagustin32@gmail.com>
2021-03-17 19:39:56 -0300

    Updates

---
+++ precache-manifest.3e40a13cbddcec74de3c2d60beb17404.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c80796425478185098fd63ae58944e",
+    "url": "/webclima%E2%80%8B/index.html"
+  },
+  {
+    "revision": "214b1f33d49738ce8967",
+    "url": "/webclima%E2%80%8B/static/css/main.5ee3b6c6.chunk.css"
+  },
+  {
+    "revision": "4c7ad1129856ecec2063",
+    "url": "/webclima%E2%80%8B/static/js/2.0f69626c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/webclima%E2%80%8B/static/js/2.0f69626c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214b1f33d49738ce8967",
+    "url": "/webclima%E2%80%8B/static/js/main.f85132f2.chunk.js"
+  },
+  {
+    "revision": "1b3b799e5c345bd6490c",
+    "url": "/webclima%E2%80%8B/static/js/runtime-main.5188fb9e.js"
+  },
+  {
+    "revision": "7a1d9431535bb65318612983ba497887",
+    "url": "/webclima%E2%80%8B/static/media/fondo negro 2.7a1d9431.jpg"
+  },
+  {
+    "revision": "a58cd0c580424eb15184959532d764fd",
+    "url": "/webclima%E2%80%8B/static/media/whatsapp.a58cd0c5.png"
+  }
+]);
